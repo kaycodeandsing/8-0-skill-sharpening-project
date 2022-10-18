11d9efcc8ff8f75cacd33e087e09450d47bdc01e
Karimah Reavis <Karimah586@gmail.com>
2022-10-18 14:25:46 -0400

    7th set of answers

--- src/06-loops.js
+++ src/06-loops.js
@@ -142,7 +142,13 @@ function firstIndexFound(array, target) {}
  *  lastIndexFound([ "left", "right", "left" ], "up");
  *  //> -1
  */
-function lastIndexFound(array, target) {}
+function lastIndexFound(array, target) {
+  for (let i = 0; i < array.length; i++){
+    if (array[array.length - i] === target){
+      return array.length - i
+    } 
+  }   return -1
+}
 
 /**
  * timesIndex()
@@ -156,8 +162,11 @@ function lastIndexFound(array, target) {}
  *  //> [ 7 * 0, 10 * 1, 11 * 2 ]
  *  //> [ 0, 10, 22 ]
  */
-function timesIndex(array) {}
-
+function timesIndex(array) {
+  for (let i = 0; i < array.length; i++){
+    array[i] *= i
+  } return array
+}
 /**
  * cumulativeSum()
  * ---------------------
@@ -170,7 +179,13 @@ function timesIndex(array) {}
  *  //> [ 5, 5 + 2, 5 + 2 + 9 ]
  *  //> [ 5, 7, 16 ]
  */
-function cumulativeSum(array) {}
+function cumulativeSum(array) {
+  let accSum = 0
+  for (let i = 0; i < array.length; i++){
+    accSum += array[i]
+     array[i] = accSum 
+  } return array
+}
 
 // Do not change anything below this line.
 module.exports = {
